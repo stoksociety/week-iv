1db52fe15b89b16c7347fe2abf407282b56eade7
stocksociety <arnold.kagunda@student.moringaschool.com>
2022-03-15 05:45:22 +0300

    add the abstract classes

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -1,4 +1,16 @@
 package dao;
 
-public class DepartmentDao {
+import models.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+    //post
+    void add(Department department);
+
+    //get
+    List<Department> getAllDeparts();
+
+    //get by id
+    Department findById(int id);
 }
--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,20 @@
 package dao;
 
-public class NewsDao {
+import models.News;
+
+import java.util.List;
+
+public interface NewsDao {
+
+    //post new news object
+    void add(News news);
+
+    //get allNewsObjects
+    List<News> getAllNews();
+
+    //get news by Id
+    News findById(int id);
+
+    //get news by depart
+    List<News> findNewsByDepart(int departId);
 }
