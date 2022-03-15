aa68c00e348f08e73b8120a43efbb331ffa480f2
stocksociety <arnold.kagunda@student.moringaschool.com>
2022-03-15 06:01:40 +0300

    add exception

--- src/main/java/exceptions/ApiException.java
+++ src/main/java/exceptions/ApiException.java
@@ -1,4 +1,11 @@
 package exceptions;
-
-public class ApiException {
+public class ApiException extends  RuntimeException {
+    private  final int statusCode;
+    public  ApiException (int statusCode, String errorMsg){
+        super(errorMsg);
+        this.statusCode = statusCode;
+    }
+    public int getStatusCode() {
+        return statusCode;
+    }
 }
